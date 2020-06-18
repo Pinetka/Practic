0502bd6314fd1e44bf2b93c27f8661bcd761c2a5
Sveta <svetik1404.89@mail.ru>
2020-06-18 22:54:55 +0400

    homework2

---
+++ cycle.js
@@ -0,0 +1,23 @@
+let num=50;
+while (num<55) {
+    console.log (num);
+    num++;
+}
+
+let num=50;
+do {
+    console.log (num);
+    num++;
+}
+while (num<55)
+
+for (let i=1; i<8; i++) {
+    console.log(i)
+}
+
+for (let i=1; i<8; i++) {
+    if (i==6) {
+        continue
+    }
+    console.log(i)
+}
